36e9e55a63faf150681f3c6aaa22e7721f405d3d
rohitrg96 <rohitg@mindfiresolutions.com>
2024-12-05 21:04:24 +0530

    user signup added

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
+import { userRoutes } from './routes/user.routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
- 
+
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -19,6 +20,8 @@ connectDB();
 app.get('/', (req: Request, res: Response) => {
   res.send('Recipe Sharing API');
 });
+
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
