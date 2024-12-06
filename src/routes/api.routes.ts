e2f316d7f3c76ae062a76b3db6643e3d07fd373b
rohitrg96 <rohitg@mindfiresolutions.com>
2024-12-06 18:59:48 +0530

    recipe add and update added

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 const apiRouter = Router();
 import userRouter from '../routes/user.routes';
 import authRouter from './auth.routes';
+import recipeRouter from './recipe.routes';
 
 apiRouter.use('/users', userRouter);
 apiRouter.use('/auth', authRouter);
+apiRouter.use('/recipes', recipeRouter);
 
 export default apiRouter;
