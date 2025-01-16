2e746f61799c0e5b58df2618d68089e8d22e80fa
rohitrg96 <rohitg@mindfiresolutions.com>
2025-01-16 10:27:16 +0530

    error modified

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,8 @@
-declare global {
-  namespace Express {
-    export interface Request {
-      user: any;
-    }
-    export interface Response {
-      user: any;
-    }
+declare namespace Express {
+  export interface Request {
+    user: any;
+  }
+  export interface Response {
+    user: any;
   }
 }
